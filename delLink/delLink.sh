a5b1ea0af2b28df44a39a3d6a62e9a68e3af17bb
Li Weiwei <15216601917@163.com>
2022-04-08 11:32:50 +0800

    add another version of script

--- delLink/delLink.sh
+++ delLink/delLink.sh
@@ -7,6 +7,13 @@ for file in "$@"; do
     sed -i '' 's/\[\[.*|\(.*\)\]\]/\1/g' "$file"
 done
 
+# Another version, this version will break the original [[]], such as picture
+# for file in "$@"; do 
+#    # \1 is group 1, the left display text
+#    sed -Ei '' 's/\[\[[0-9a-zA-Z-]+\|//g' "$file"
+#    sed -Ei '' 's/\]\]//g' "$file"
+# done
+
 
 # Example:
 # $ sh deleteObsidianLink.sh file.md
