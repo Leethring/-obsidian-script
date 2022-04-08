c2e6ca288607ab44aeaadd3dc3086ba0591a09eb
Li Weiwei <15216601917@163.com>
2022-04-08 22:51:55 +0800

    add moveTag.sh

---
+++ src/moveTag.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+# moveTag.sh moves the line of tag to the end of a file.
+
+for file in "$@"; do
+    echo "\n" >> $file;
+    grep '#[a-zA-Z]' "$file" | head -n 1 >> "$file";
+    gsed -i '/#[a-zA-Z]/{2,3d}' "$file"
+done
+
+# sed '/#[a-zA-Z]/d' test.md
+
+
