18862427efd99f91d8fe9588500df715a495f187
Li Weiwei <15216601917@163.com>
2022-04-20 10:54:30 +0800

    blank line can be add to title

--- src/addTitle.sh
+++ src/addTitle.sh
@@ -2,8 +2,8 @@
 # addTitle.sh add filename to the start of the note file
 
 for file in "$@"; do 
-    sed -Ei '' "1 s~(^.)~# $file\n\1~g" "$file"; # Add relative path to head of file 
-    sed -Ei '' "1 s~# .*\/~# ~g" "$file";
+    sed -Ei '' "1 s~(^.)~# $file\n\1~g" "$file"; # Add relative path to head if it's blank
+    sed -Ei '' "1 s~^$~# $file~g" "$file"; # Add relative path to head of file 
     sed -Ei '' '1 s/[0-9/.]//g' "$file"; # remove digit, slash and dot of path
     sed -Ei '' '1 s/-the-//g' "$file"; # remove the first key words
     sed -Ei '' '1 s/-those-//g' "$file"; # remove those first key words
