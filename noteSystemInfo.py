1b57bcfe6ebe6c10b6a0715d8f69e2daefdcdb67
Li Weiwei <15216601917@163.com>
2022-09-11 10:52:58 +0800

    adjust function order following sort order

--- noteSystemInfo.py
+++ noteSystemInfo.py
@@ -8,58 +8,74 @@ def day(notes):
     """show day information of notes."""
     pass
 
-def walkNotes(f):
-    """Counts the number of notes within a directory.
+def folderInfo(f):
+    """Print All information about this folder
     
-    f: the path of this note folder
+    f: Absolute path of a folder
     """
-    # Find all files in this folder
-    notes = []
-    for foldername, subfolders, filenames in os.walk(f):
-        for filename in filenames:
-            if filename.endswith('.md'):
-                notes.append(filename)
-    print(f'The total number of notes in this directory is:')
-    return notes
+    print(f'We are in folder: {os.path.basename(f)}')
+    showYear(f)
+    # Get all notes in a note directory
+    notes = walkNotes(f)
+    print(len(notes), end='\n\n')
+    # Info about year
+    yearDict = year(notes)
+    # Info about month
+    month(notes, yearDict)
+#   day(notes)
 
-def year(notes):
-    """Show the number of notes within a year.
+def getPath(choice, path):
+    """Return the absolute path of given folder.
     
-    notes: A list includes all notes
+    choice: Choice of folder name
+    path: Path of note system
     """
-    # Dictionary to store year-numberOfNotes pairs
-    yearDict = {}
-    # We start write note from 2018 
-    startYear = 2018
-    # Init year dictionary
-    for i in range(5):
-        yearDict[str(startYear + i)] = 0
-    # Record correct year-notes pair
-    for i in range(5):
-        yearCounter = 0
-        for note in notes:
-            if note.startswith(str(startYear+ i)):
-                yearCounter = yearCounter + 1
-        yearDict[str(startYear +i)] = yearCounter
-    print(f'Every year include different numbers of note:\n{yearDict}\n')
-    # Find the max number of adding within a year 
-    maxNumNote = max(yearDict.values())
-    maxYear = {i for i in yearDict if yearDict[i]==maxNumNote}
-    print(f'The max number of notes a year is: {maxNumNote} in {list(maxYear)[0]} year\n')
-    # Calculate average notes of this year
-    yearSum = 0
-    yearAverage = 0
-    # When a year don't have any notes, we don't average this year
-    countYear = 0
-    for value in yearDict.values():
-        if value == 0:
-            continue
-        else:
-            yearSum += value
-            countYear += 1
-    yearAverage = yearSum // countYear
-    print(f'The average of adding notes every year is: {yearAverage}\n')
-    return yearDict
+    # Find all notes folders
+    myNotesAbs = os.path.join(path, 'my_notes')
+    projectsAbs = os.path.join(path, 'Projects')
+    myNotesFolders = os.listdir(myNotesAbs)
+    # Return absolute path of the chosen folder
+    if choice in myNotesFolders:
+        return os.path.join(myNotesAbs, choice)
+    else:
+        return os.path.join(projectsAbs, choice)
+
+def interfaceFolder(p):
+    """Shows interface.
+    
+    p: The absolute path of current folder
+    """
+    cwdBasename = os.path.basename(p)
+    print(f'We are in {cwdBasename} folder')
+    print("""
+    Please choose a folder to check its information (Input its letter):
+    (Quit when typing others)
+    """)
+    # Find all note folders in current folder
+    my_notes = os.path.join(p, 'my_notes')
+    projects = os.path.join(p, 'Projects')
+    dirListMyNotes = os.listdir(my_notes)
+    dirListProjects = os.listdir(projects)
+    dirListRaw = dirListMyNotes + dirListProjects
+    dirList = []
+    # Delete folders or files not are note folders
+    for i in dirListRaw:
+        if '.' not in i:
+           dirList.append(i) 
+    startChar = 65
+    # Create Choices
+    listChar = []
+    for i in range(len(dirList)):
+        listChar.append(chr(startChar + i))
+    # Create choice-folder pairs
+    dirDict = {}
+    for i in range(len(dirList)):
+        dirDict[listChar[i]] = dirList[i]
+    # Show choice of folders
+    for key, value in dirDict.items():
+        print(f'{key}. {value}')
+    return dirDict
+
 
 def month(notes, yearDict):
     """Show month information about notes.
@@ -130,74 +146,58 @@ def showYear(f):
         print(os.path.basename(year), end=' ')
     print("\n")
 
-def folderInfo(f):
-    """Print All information about this folder
+def walkNotes(f):
+    """Counts the number of notes within a directory.
     
-    f: Absolute path of a folder
+    f: the path of this note folder
     """
-    print(f'We are in folder: {os.path.basename(f)}')
-    showYear(f)
-    # Get all notes in a note directory
-    notes = walkNotes(f)
-    print(len(notes), end='\n\n')
-    # Info about year
-    yearDict = year(notes)
-    # Info about month
-    month(notes, yearDict)
-#   day(notes)
+    # Find all files in this folder
+    notes = []
+    for foldername, subfolders, filenames in os.walk(f):
+        for filename in filenames:
+            if filename.endswith('.md'):
+                notes.append(filename)
+    print(f'The total number of notes in this directory is:')
+    return notes
 
-def getPath(choice, path):
-    """Return the absolute path of given folder.
+def year(notes):
+    """Show the number of notes within a year.
     
-    choice: Choice of folder name
-    path: Path of note system
+    notes: A list includes all notes
     """
-    # Find all notes folders
-    myNotesAbs = os.path.join(path, 'my_notes')
-    projectsAbs = os.path.join(path, 'Projects')
-    myNotesFolders = os.listdir(myNotesAbs)
-    # Return absolute path of the chosen folder
-    if choice in myNotesFolders:
-        return os.path.join(myNotesAbs, choice)
-    else:
-        return os.path.join(projectsAbs, choice)
-
-def interfaceFolder(p):
-    """Shows interface.
-    
-    p: The absolute path of current folder
-    """
-    cwdBasename = os.path.basename(p)
-    print(f'We are in {cwdBasename} folder')
-    print("""
-    Please choose a folder to check its information (Input its letter):
-    (Quit when typing others)
-    """)
-    # Find all note folders in current folder
-    my_notes = os.path.join(p, 'my_notes')
-    projects = os.path.join(p, 'Projects')
-    dirListMyNotes = os.listdir(my_notes)
-    dirListProjects = os.listdir(projects)
-    dirListRaw = dirListMyNotes + dirListProjects
-    dirList = []
-    # Delete folders or files not are note folders
-    for i in dirListRaw:
-        if '.' not in i:
-           dirList.append(i) 
-    startChar = 65
-    # Create Choices
-    listChar = []
-    for i in range(len(dirList)):
-        listChar.append(chr(startChar + i))
-    # Create choice-folder pairs
-    dirDict = {}
-    for i in range(len(dirList)):
-        dirDict[listChar[i]] = dirList[i]
-    # Show choice of folders
-    for key, value in dirDict.items():
-        print(f'{key}. {value}')
-    return dirDict
-
+    # Dictionary to store year-numberOfNotes pairs
+    yearDict = {}
+    # We start write note from 2018 
+    startYear = 2018
+    # Init year dictionary
+    for i in range(5):
+        yearDict[str(startYear + i)] = 0
+    # Record correct year-notes pair
+    for i in range(5):
+        yearCounter = 0
+        for note in notes:
+            if note.startswith(str(startYear+ i)):
+                yearCounter = yearCounter + 1
+        yearDict[str(startYear +i)] = yearCounter
+    print(f'Every year include different numbers of note:\n{yearDict}\n')
+    # Find the max number of adding within a year 
+    maxNumNote = max(yearDict.values())
+    maxYear = {i for i in yearDict if yearDict[i]==maxNumNote}
+    print(f'The max number of notes a year is: {maxNumNote} in {list(maxYear)[0]} year\n')
+    # Calculate average notes of this year
+    yearSum = 0
+    yearAverage = 0
+    # When a year don't have any notes, we don't average this year
+    countYear = 0
+    for value in yearDict.values():
+        if value == 0:
+            continue
+        else:
+            yearSum += value
+            countYear += 1
+    yearAverage = yearSum // countYear
+    print(f'The average of adding notes every year is: {yearAverage}\n')
+    return yearDict
 if __name__ == "__main__":
     noteSystemFolder = '/Users/liweiwei/Nutstore Files/note_system'
     # Show the interface
