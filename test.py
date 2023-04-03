1d6a2b2d376e805ee65cf8a7b067f3edee54f165
Li Weiwei <15216601917@163.com>
2023-04-03 11:57:55 +0800

    add frontmatter

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import fileinput 
 import sys
-for line in fileinput.input():
-    print(line, end='')
+for line in fileinput.input('test2.txt', inplace=True):
+    if line.startswith('Example:'):
+        line = line.replace('Example:', '[!example]')
+    print(line,end='')
